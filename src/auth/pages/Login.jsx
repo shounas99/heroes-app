60d0984ed42193898bf2554cf9771604989da673
Cristina Aguilar Rodriguez <to.aguilar.rodriguez.cristina@gmail.com>
2025-04-04 14:08:03 -0600

    feat: add navigate
    - use useNavigate to navige between routes and don't show the history

--- src/auth/pages/Login.jsx
+++ src/auth/pages/Login.jsx
@@ -1,5 +1,26 @@
+import { useNavigate } from "react-router-dom"
+
 export const Login = () => {
+  const navigate = useNavigate()
+
+  const onLogin = () => {
+    navigate('/', {
+      replace: true // don't show the history
+    })
+  }
+
   return (
-    <h1>Login Page</h1>
+    <div className="container mt-5">
+      <h1>Login</h1>
+      <hr />
+
+      <button 
+        onClick={ onLogin }
+        className="btn btn-primary"
+      >
+        Login
+      </button>
+
+    </div>
   )
 }
