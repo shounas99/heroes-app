301268780819f2597bff3903f188a3386f3a42db
Cristina Aguilar Rodriguez <to.aguilar.rodriguez.cristina@gmail.com>
2025-04-09 14:51:02 -0600

    feat: add conditionals to show messages
    - add alert messages

--- src/heroes/pages/Search.jsx
+++ src/heroes/pages/Search.jsx
@@ -11,8 +11,10 @@ export const Search = () => {
   const location = useLocation() //get info from the location where I am
 
   const { q = '' } = queryString.parse( location.search )
-
   const heroes = getHeroesByName(q)
+
+  const showSearch = (q.length === 0)
+  const showError = (q.length > 0) && heroes.length === 0
 
   const { searchText, onInputChange } = useForm({
     searchText: q,
@@ -20,7 +22,7 @@ export const Search = () => {
 
   const onSearchSubmit = ( event ) => {
     event.preventDefault()
-    if( searchText.trim().length <= 1) return;
+    // if( searchText.trim().length <= 1) return;
 
     navigate(`?q=${ searchText }`)
   }
@@ -56,12 +58,16 @@ export const Search = () => {
           <h4>Results</h4>
           <hr />
 
-          <div className="alert alert-primary">
-            Search a hero
-          </div>
-          <div className="alert alert-danger">
-            No hero with <b>{ q }</b>
-          </div>
+          {/* {
+            ( q === ''
+              ? ( <div className="alert alert-primary">Search a hero</div> )
+              : ( heroes.length === 0)
+              ? ( <div className="alert alert-danger"> No hero with <b>{ q }</b> </div> )
+              : ''
+              )
+              } */}
+          <div className="alert alert-primary animate__animated animate__fadeIn" style={{ display: showSearch ? '' : 'none'}}>Search a hero</div>
+          <div className="alert alert-danger animate__animated animate__fadeIn" style={{ display: showError ? '' : 'none'}}> No hero with <b>{ q }</b> </div> 
 
           {
             heroes.map( hero => (
